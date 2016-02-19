540e90b65250ff6ffd24132f0b0659e6ddf677f1
AIZAWA Hina <hina@bouhime.com>
2016-02-19 21:12:16 +0900

    app config
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ config/slack.sample.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'token' => 'xoxp-00000000000-11111111111-22222222222-3333333333',
+    'postMessage' => [
+        'channel'       => '#email',
+        'username'      => 'Received Email',
+        'icon_emoji'    => ':envelope_with_arrow:',
+    ],
+];
